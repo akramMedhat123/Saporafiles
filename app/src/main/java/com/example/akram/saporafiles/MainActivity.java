c243441ec70aeb529d531853b59497ee598b6f90
akram medhat <su.1204012@su.edu.eg>
2016-03-13 01:04:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/akram/saporafiles/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.akram.saporafiles;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"there is no intenet connection ",Toast.LENGTH_LONG).show();
+
+    }
+
+    public void upload(View v){
+
+        Toast.makeText(this,"please connect to the internet",Toast.LENGTH_LONG).show();
+
+
+    }
+    public void doenload(View v){
+
+        Toast.makeText(this,"please connect to the internet",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
